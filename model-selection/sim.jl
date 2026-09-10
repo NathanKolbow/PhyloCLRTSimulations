71e4bc310570f97652a7a424161af0f8634fc922
Nathan Kolbow <nkolbow@wisc.edu>
2026-09-10 12:11:10 -0500

    updated sims

--- model-selection/sim.jl
+++ model-selection/sim.jl
@@ -1,4 +1,3 @@
-@error "Try adjusting variability: sum(dlogf) * sum(dlogf)' instead of sum(dlogf * dlogf') and sensitivity sum(dlogf) * sum(dlogf)' instead of using second derivative"
 Threads.nthreads() > 1 || @warn "Only $(Threads.nthreads()) threads are in use."
 
 length(ARGS) > 0 || error("Must provide 1 command line argument for the value of ϵ (0, 0.0001, 0.001, 0.01, 0.1, 0.5, or 1.0)")
